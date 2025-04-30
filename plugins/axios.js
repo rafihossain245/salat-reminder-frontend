87ff9d9580bd7b54736e8db49c096e5364c2c308
Rafi Hossain <arrafi245@gmail.com>
2025-05-01 00:07:39 +0600

    base url update

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: process.env.PUBLIC_API_BASE_URL || 'http://salat-notifier-api.test/api',
+    baseURL: process.env.PUBLIC_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
